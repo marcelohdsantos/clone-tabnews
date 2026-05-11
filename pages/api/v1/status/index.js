5f31eca8addfb0519b90b8e94fe7a083eabfa941
Marcelo Henrique dos Santos <46683705+marcelohdsantos@users.noreply.github.com>
2026-05-11 23:53:18 +0000

    add  file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum");
-  //console.log("<result------->:", result.rows);
   res.status(200).json({ status: "OK" });
 }
 export default status;
